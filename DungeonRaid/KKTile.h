9835e428b0168c323aacdcbcbdf952ff0ee258d3
kkgn06 <k.kennichi.knlnshnm@gmail.com>
2013-03-09 17:45:10 +0900

    TileクラスをKKTileにリネーム

---
+++ DungeonRaid/KKTile.h
@@ -0,0 +1,26 @@
+//
+// Created by kkgn06 on 2013/03/09.
+//
+// To change the template use AppCode | Preferences | File Templates.
+//
+
+
+#import <Foundation/Foundation.h>
+
+//typedef enum {
+//    coin,
+//    enemy,
+//    potion,
+//    shield,
+//    sword,
+//    TileType_MAX,
+//} TileType;
+
+@interface KKTile : CCNode
+
+//+ (id)tileWithType:(TileType)type;
+//
+//- (id)initWithType:(TileType)type;
+//
+//@property(nonatomic) TileType type;
+@end
---
+++ DungeonRaid/KKTile.m
@@ -0,0 +1,49 @@
+//
+// Created by kkgn06 on 2013/03/09.
+//
+// To change the template use AppCode | Preferences | File Templates.
+//
+
+
+#import "KKTile.h"
+
+
+@implementation KKTile {
+
+}
+//
+//+ (id)tileWithType:(TileType)type {
+//    return [[[self alloc] initWithType:type] autorelease];
+//}
+//
+//- (id)initWithType:(TileType)type {
+//    self = [super init];
+//    if (self) {
+//        _type = type;
+//
+//        NSString *fileName;
+//        switch (self.type) {
+//            case coin:
+//                fileName = @"coin.png";
+//                break;
+//            case enemy:
+//                fileName = @"enemy.png";
+//                break;
+//            case potion:
+//                fileName = @"potion.png";
+//                break;
+//            case shield:
+//                fileName = @"shield.png";
+//                break;
+//            case sword:
+//                fileName = @"sword.png";
+//                break;
+//            case TileType_MAX:
+//                NSAssert(NO, @"このTileTypeはまだ実装されてない");
+//                break;
+//        }
+//    }
+//    return self;
+//}
+
+@end
