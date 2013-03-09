55bc0f540712ca7b3b479ae0aab548bb6df10f4b
kkgn06 <k.kennichi.knlnshnm@gmail.com>
2013-03-09 15:44:19 +0900

    Tileクラス追加

---
+++ DungeonRaid/Tile.h
@@ -0,0 +1,12 @@
+//
+// Created by kkgn06 on 2013/03/09.
+//
+// To change the template use AppCode | Preferences | File Templates.
+//
+
+
+#import <Foundation/Foundation.h>
+
+
+@interface Tile : NSObject
+@end
