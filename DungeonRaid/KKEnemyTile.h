ac1626868c4f4951ee74ad08fce2261280bb4233
kkgn06 <k.kennichi.knlnshnm@gmail.com>
2013-04-05 00:34:03 +0900

    敵のTileクラスを追加

---
+++ DungeonRaid/KKEnemyTile.h
@@ -0,0 +1,13 @@
+//
+// Created by kkgn06 on 2013/04/05.
+//
+// To change the template use AppCode | Preferences | File Templates.
+//
+
+
+#import <Foundation/Foundation.h>
+#import "KKTile.h"
+
+
+@interface KKEnemyTile : KKTile 
+@end
---
+++ DungeonRaid/KKEnemyTile.m
@@ -0,0 +1,14 @@
+//
+// Created by kkgn06 on 2013/04/05.
+//
+// To change the template use AppCode | Preferences | File Templates.
+//
+
+
+#import "KKEnemyTile.h"
+
+
+@implementation KKEnemyTile {
+
+}
+@end
